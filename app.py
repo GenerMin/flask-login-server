a4e3426f239b925be330521d8717796074982df0
GenerMin <alawo10165@gmail.com>
2025-05-15 03:19:19 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user_id = data.get('id')
+    user_pw = data.get('pw')
+
+    print(f"Received ID: {user_id}, PW: {user_pw}")
+
+    if user_id == 'admin' and user_pw == '1234':
+        return jsonify(status='success', message='Login Success')
+    else:
+        return jsonify(status='fail', message='Login Failed')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
